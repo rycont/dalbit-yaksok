ce5099ea11405fb6e02c330f08aaee71db7b7efe
정한 Rycont <35295182+rycont@users.noreply.github.com>
2025-01-11 00:27:55 +0000

    chore(monaco-language-provider): export nodeToColorTokens

---
+++ monaco-language-provider/language.ts
@@ -0,0 +1,71 @@
+import { languages, editor } from 'monaco-editor'
+import { LANG_ID } from './const.ts'
+import { BaseProvider } from './provider/base.ts'
+import {
+    CompletionItemProvider,
+    setupCompletion,
+} from './provider/completion-item.ts'
+import { TokensProvider } from './provider/tokens.ts'
+
+export class DalbitYaksokApplier {
+    baseProvider: BaseProvider
+
+    tokensProvider: TokensProvider
+    completionItemProvider: CompletionItemProvider
+
+    constructor(initialCode: string) {
+        this.baseProvider = new BaseProvider(initialCode)
+
+        this.tokensProvider = new TokensProvider(this.baseProvider)
+        this.completionItemProvider = new CompletionItemProvider(
+            this.baseProvider,
+        )
+    }
+
+    public async register(languagesInstance: typeof languages) {
+        languagesInstance.register({ id: LANG_ID })
+
+        await new Promise<void>((resolve) =>
+            languagesInstance.onLanguage(LANG_ID, resolve),
+        )
+
+        languagesInstance.setLanguageConfiguration(LANG_ID, {
+            comments: {
+                lineComment: '#',
+            },
+            brackets: [
+                ['{', '}'],
+                ['[', ']'],
+                ['(', ')'],
+            ],
+            autoClosingPairs: [
+                { open: '{', close: '}' },
+                { open: '[', close: ']' },
+                { open: '(', close: ')' },
+                { open: '"', close: '"', notIn: ['string'] },
+                { open: "'", close: "'", notIn: ['string'] },
+            ],
+            surroundingPairs: [
+                { open: '{', close: '}' },
+                { open: '[', close: ']' },
+                { open: '(', close: ')' },
+                { open: '"', close: '"' },
+                { open: "'", close: "'" },
+            ],
+        })
+
+        languagesInstance.setTokensProvider(LANG_ID, this.tokensProvider)
+        languagesInstance.registerCompletionItemProvider(
+            LANG_ID,
+            this.completionItemProvider,
+        )
+    }
+
+    public configAutocomplete(editorInstance: editor.IStandaloneCodeEditor) {
+        setupCompletion(editorInstance)
+    }
+
+    updateCode(code: string) {
+        this.baseProvider.updateCode(code)
+    }
+}
