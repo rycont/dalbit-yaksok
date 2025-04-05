d7bdc59f1a132d8b69f1832c6e0634bb4806350e
정한 Rycont <35295182+rycont@users.noreply.github.com>
2025-04-05 04:51:27 +0000

    docs: add document for baseContext

--- runtest.ts
+++ runtest.ts
@@ -1,28 +1,10 @@
 import { CodeFile, yaksok } from '@dalbit-yaksok/core'
 
-const baseContext: CodeFile = (
-    await yaksok(`
-약속, 지금/현재 시간 가져오기/말하기
+const baseCode = `
+약속, 힘든 부탁 하기
     "그건 제가 도와드릴 수 없어요" 보여주기
+`
 
-약속, 지금/현재/지금의 밀리초 가져오기/말하기
-    "그것도 제가 도와드릴 순 없어요.." 보여주기
-    10 / "예?" 보여주기
-`)
-).codeFiles['main']
+const baseContext: CodeFile = (await yaksok(baseCode)).codeFiles['main']
 
-await yaksok(
-    `지금 시간 가져오기
-지금 시간 말하기
-현재 시간 가져오기
-현재 시간 말하기
-
-지금 밀리초 가져오기
-지금 밀리초 말하기
-현재 밀리초 가져오기
-현재 밀리초 말하기
-지금의 밀리초 가져오기
-지금의 밀리초 말하기`,
-    {},
-    baseContext,
-)
+await yaksok(`힘든 부탁 하기`, {}, baseContext)
