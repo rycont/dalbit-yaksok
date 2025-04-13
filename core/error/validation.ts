edbfa05da4b1da0193f7cfe5bcbd31a7f5ff4e0b
정한 Rycont <35295182+rycont@users.noreply.github.com>
2025-04-13 02:22:17 +0000

    fix: make ErrorGroups to inherit Error class

--- core/error/validation.ts
+++ core/error/validation.ts
@@ -1,5 +1,8 @@
 import { YaksokError } from './common.ts'
 
-export class ErrorGroups {
-    constructor(public errors: Map<string, YaksokError[]>) {}
+export class ErrorGroups extends Error {
+    constructor(public errors: Map<string, YaksokError[]>) {
+        super('여러 오류가 발생했습니다.')
+        this.name = 'ErrorGroups'
+    }
 }
