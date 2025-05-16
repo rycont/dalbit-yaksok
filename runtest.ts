d7e4ad01f9380d27f282ab095ae93c0c1ecf5ff9
정한 Rycont <35295182+rycont@users.noreply.github.com>
2025-05-16 09:05:27 +0000

    feat: support detecting breaker in loop

--- runtest.ts
+++ runtest.ts
@@ -1,14 +1,4 @@
 import { yaksok } from '@dalbit-yaksok/core'
 
-await yaksok({
-    하랑봇: `
-약속, (내용) 말하기
-    내용 + "? 어쩌라고." 보여주기
-
-날씨 = "비"
-`,
-    main: `
-할말 = "뭐라고"
-@하랑봇 (@하랑봇 날씨) 말하기
-`,
-})
+await yaksok(`반복
+    1 + 1 보여주기`)
