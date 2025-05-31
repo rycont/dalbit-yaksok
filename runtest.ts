3747107ae44f5c51b8d1ac6fb2931df47a691e7c
rycont <reactdev@kakao.com>
2025-05-31 15:04:00 +0900

    chore: bump package versions to 1.3.3-RC.1

--- runtest.ts
+++ runtest.ts
@@ -2,9 +2,12 @@ import { yaksok } from '@dalbit-yaksok/core'
 
 await yaksok(`
 자리 = [
-    1,
-    2,
-    3
+    [ 1, 2, 3 ],
+    [ 4, 5, 6 ],
+    [ 7, 8, 9 ],
+    [
+        10, 11, 12
+    ]
 ]
 자리 보여주기
 `)
