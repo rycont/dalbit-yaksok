35d61bce2fa6f191b5daa6d050e197fa4b2e83fc
정한 Rycont <35295182+rycont@users.noreply.github.com>
2025-06-28 05:21:36 +0000

    chore: update vitepress / deno vite loader version

--- runtest.ts
+++ runtest.ts
@@ -1,14 +1,34 @@
 import { YaksokSession } from '@dalbit-yaksok/core'
 
-const session = new YaksokSession();
-await session.run(`
-자리 = [
-    [ 1, 2, 3 ],
-    [ 4, 5, 6 ],
-    [ 7, 8, 9 ],
-    [
-        10, 11, 12
-    ]
-]
-자리 보여주기
-`)
+const session = new YaksokSession()
+
+await session.runModule(
+    '차종',
+    `
+KTX이음 = "KTX-이음"
+무궁화호 = "무궁화호"
+`,
+)
+
+await session.runModule(
+    '코레일',
+    `
+약속, (차종)으로 (거리)km을 이동할 때 운임
+    만약 차종 == @차종 KTX이음 이면
+        만약 거리 <= 60 이면
+            8400 반환하기
+        아니면
+            8400 + (거리 - 60) * 140.2 반환하기
+
+    만약 차종 == @차종 무궁화호 이면
+        만약 거리 <= 40 이면
+            2600 반환하기
+        아니면
+            2600 + (거리 - 40) * 65 반환하기
+
+약속, 출발하기
+    "빵빵" 보여주기
+`,
+)
+
+await session.run('@코레일 출발하기')
