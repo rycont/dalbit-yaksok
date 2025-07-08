825ac9db4b6e07773d3cf1b57e80ad55789d836b
정한 Rycont <35295182+rycont@users.noreply.github.com>
2025-07-08 14:55:44 +0000

    feat: add feature flag to skip validation of break/return in loops

--- core/constant/feature-flags.ts
+++ core/constant/feature-flags.ts
@@ -3,6 +3,10 @@ export enum FEATURE_FLAG {
      * 약속 호출에서 신문법 사용을 강제합니다. 신문법에서는 인자에 괄호를 사용해야만 합니다. 예를 들어, `("치킨")먹기`는 가능하지만 `"치킨" 먹기`는 불가능합니다.
      */
     FUTURE_FUNCTION_INVOKE_SYNTAX = 'future-function-invoke-syntax',
+    /**
+     * "반복" 구문 내에 멈춤 코드(약속 그만, 반복 그만, 돌려주기)가 없어도 유효성 검사에 실패하지 않습니다.
+     */
+    SKIP_VALIDATE_BREAK_OR_RETURN_IN_LOOP = 'skip-validate-break-or-return-in-loop',
 }
 
 export type EnabledFlags = Partial<Record<FEATURE_FLAG, boolean>>
--- runtest.ts
+++ runtest.ts
@@ -6,26 +6,16 @@ const quickjs = new QuickJS({
 })
 
 await quickjs.init()
-
 await yaksok(
     `
-번역(JavaScript), (arr) (n)번째 값 제거
-***
-return [...arr.slice(0, n), ...arr.slice(n + 1)];
-***
-
-배열 = [1, 2, 3, 4, 5]
-배열 보여주기
-배열[2] 보여주기
-
-삭제된거 = 배열 2번째 값 제거
-삭제된거 보여주기
-삭제된거[2] 보여주기
-배열 보여주기
-`,
+순서 = 0
+반복
+    순서 = 순서 + 1
+    만약 순서 == 3 이면
+        [] / 2 보여주기`,
     {
-        runFFI(_, code, args) {
-            return quickjs.run(code, args)
+        flags: {
+            'skip-validate-break-or-return-in-loop': true,
         },
     },
 )
