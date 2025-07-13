32c799010f61c6cd9f9a59f2e8055fe0f742817e
정한 Rycont <35295182+rycont@users.noreply.github.com>
2025-07-13 18:25:53 +0900

    Add codebook (#111)
    * feat: integrate Mermaid and update sidebar configuration in VitePress
    
    * docs: add codebook
    
    * docs: GEMINI.md
    
    * feat: add Copilot setup workflow for automated testing and validation
    
    * feat: update features section in index.md and remove deprecated actions
    
    * feat: add Contribution link to sidebar and update code references in documentation
    
    * refactor:: rename codebook to guide
    
    * fix: correct title in introduction from "가이드" to "기여 가이드"

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,6 +1,7 @@
-import { withSidebar } from 'vitepress-sidebar'
 import pluginDeno from '@deno/vite-plugin'
 import { defineConfig } from 'vitepress'
+import { withMermaid } from 'vitepress-plugin-mermaid'
+import { withSidebar } from 'vitepress-sidebar'
 
 import type { VitePressSidebarOptions } from 'vitepress-sidebar/types'
 
@@ -16,61 +17,70 @@ const workspacePath = new URL('../..', import.meta.url).pathname
 console.log({ workspacePath })
 
 export default defineConfig(
-    withSidebar(
-        {
-            title: '달빛약속',
-            description: '교육용 프로그래밍 언어, 약속의 포크',
-            themeConfig: {
-                nav: [
-                    { text: 'Home', link: '/' },
-                    {
-                        text: 'API',
-                        link: '/api/',
-                    },
-                    {
-                        text: 'Language',
-                        link: '/language/1.%20getting-started.html',
-                    },
-                    {
-                        text: 'Library',
-                        link: '/library/1.%20getting-started.html',
-                    },
-                ],
-                socialLinks: [
-                    {
-                        icon: 'github',
-                        link: 'https://github.com/rycont/yaksok.ts',
-                    },
-                    {
-                        icon: {
-                            svg: '<svg viewBox="0 0 13 7" aria-hidden="true" height="28"><path d="M0,2h2v-2h7v1h4v4h-2v2h-7v-1h-4" fill="#083344"></path><g fill="#f7df1e"><path d="M1,3h1v1h1v-3h1v4h-3"></path><path d="M5,1h3v1h-2v1h2v3h-3v-1h2v-1h-2"></path><path d="M9,2h3v2h-1v-1h-1v3h-1"></path></g></svg>',
+    withMermaid(
+        withSidebar(
+            {
+                title: '달빛약속',
+                description: '교육용 프로그래밍 언어, 약속의 포크',
+                themeConfig: {
+                    nav: [
+                        { text: 'Home', link: '/' },
+                        {
+                            text: 'API',
+                            link: '/api/',
                         },
-                        link: 'https://jsr.io/@dalbit-yaksok/core',
+                        {
+                            text: 'Language',
+                            link: '/language/1.%20getting-started.html',
+                        },
+                        {
+                            text: 'Library',
+                            link: '/library/1.%20getting-started.html',
+                        },
+                        {
+                            text: 'Contribution',
+                            link: '/codebook/00.introduction.html',
+                        },
+                    ],
+                    socialLinks: [
+                        {
+                            icon: 'github',
+                            link: 'https://github.com/rycont/yaksok.ts',
+                        },
+                        {
+                            icon: {
+                                svg: '<svg viewBox="0 0 13 7" aria-hidden="true" height="28"><path d="M0,2h2v-2h7v1h4v4h-2v2h-7v-1h-4" fill="#083344"></path><g fill="#f7df1e"><path d="M1,3h1v1h1v-3h1v4h-3"></path><path d="M5,1h3v1h-2v1h2v3h-3v-1h2v-1h-2"></path><path d="M9,2h3v2h-1v-1h-1v3h-1"></path></g></svg>',
+                            },
+                            link: 'https://jsr.io/@dalbit-yaksok/core',
+                        },
+                    ],
+                    search: {
+                        provider: 'local',
                     },
-                ],
-                search: {
-                    provider: 'local',
+                },
+                vite: {
+                    plugins: [pluginDeno()],
+                    server: {
+                        fs: {
+                            allow: [workspacePath],
+                        },
+                    },
+                    build: {
+                        rollupOptions: {
+                            watch: {
+                                include: [workspacePath],
+                            },
+                        },
+                    },
+                    ssr: {
+                        noExternal: ['monaco-editor'],
+                    },
+                    optimizeDeps: {
+                        include: ['mermaid', 'dayjs'],
+                    },
                 },
             },
-            vite: {
-                plugins: [pluginDeno()],
-                server: {
-                    fs: {
-                        allow: [workspacePath],
-                    },
-                },
-                build: {
-                    rollupOptions: {
-                        watch: {
-                            include: [workspacePath],
-                        },
-                    },
-                },
-                ssr: {
-                    noExternal: ['monaco-editor'],
-                },
-            },
-        },
-        SIDEBAR_CONFIG,
+            SIDEBAR_CONFIG,
+        ),
     ),
 )
