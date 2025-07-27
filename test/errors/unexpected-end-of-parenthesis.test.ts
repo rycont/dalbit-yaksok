772c7ca51bac720bdc8722af16d3fcdb1dc63bd5
rycont <reactdev@kakao.com>
2025-07-27 12:06:17 +0900

    chore: update outdated tests

--- test/errors/unexpected-end-of-parenthesis.test.ts
+++ test/errors/unexpected-end-of-parenthesis.test.ts
@@ -3,7 +3,7 @@ import { assert } from 'assert/assert'
 import { NotDefinedIdentifierError, yaksok } from '../../core/mod.ts'
 
 Deno.test('끝나지 못한 괄호', async () => {
-    const result = await yaksok(`나이: 10 + (20`)
+    const result = await yaksok(`나이 = 10 + (20`)
     assert(result.reason === 'validation')
     assertIsError(result.errors.get('main')![0], NotDefinedIdentifierError)
 })
