d21ae09b95841e7a27d516253fb904848092877f
rycont <reactdev@kakao.com>
2025-12-11 14:23:50 +0900

    Refactor runtest.ts to improve data analysis syntax and enhance module context handling. Update sorting phrases in mod.ts for consistency in translation.

--- runtest.ts
+++ runtest.ts
@@ -4,11 +4,13 @@ import { 편의점 } from "./data-analyze/virtual-data/편의점.ts";
 
 const session = new YaksokSession()
 
-await session.extend(new DataAnalyze())
+const dataAnalyze = new DataAnalyze()
+await session.extend(dataAnalyze)
+await session.setBaseContext(dataAnalyze.manifest.module['분석'])
 
 session.addModule("main", `
 편의점_상품들 = @데이터_불러오기 편의점 데이터
-@분석 편의점_상품들 중 '상품ID'가 'CVS003'인 값 보여주기
-@분석 (@분석 편의점_상품들 중 '가격'이 2000 이상인 것들) '재고' 순서로 정렬 보여주기
+편의점_상품들중 '상품ID'가 'CVS003'인 값 보여주기
+편의점_상품들중 '가격'이 2000 이상인 것들 '재고' 순서로 정렬해서 보여주기
 `)
 await session.runModule('main')
