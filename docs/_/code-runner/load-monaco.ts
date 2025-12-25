1735e73c0a163294a3cffe53fcc704b0d5b6373b
rycont <reactdev@kakao.com>
2025-12-25 18:22:39 +0900

    feat: enhance code completion and editor suggestions
    - Added custom action to trigger suggestion window in the Monaco editor.
    - Improved suggestion triggering based on user input changes, ensuring suggestions appear for valid character entries.
    - Updated completion item provider to utilize phoneme mapping for better Hangul support.
    - Refactored code to streamline editor instance interactions and suggestion logic.

--- docs/_/code-runner/load-monaco.ts
+++ docs/_/code-runner/load-monaco.ts
@@ -1,10 +1,10 @@
-let monaco: typeof import('monaco-editor/esm/vs/editor/editor.api')
+let monaco: typeof import('monaco-editor')
 
 export async function loadMonaco() {
     if (monaco) {
         return monaco
     }
 
-    monaco = await import('monaco-editor/esm/vs/editor/editor.api')
+    monaco = await import('monaco-editor')
     return monaco
 }
